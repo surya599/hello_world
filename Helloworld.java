1d6e2fec150f5f64976cb2e17189ae043c2c08a6
surya599 <124504865+surya599@users.noreply.github.com>
2026-02-13 09:39:31 +0530

    Rename class and add greeting for Jenkins

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
-public class Helloworldjenkins{
+public class Helloworld{
   public static void main(String args[]){
     System.out.println("Hello World");
+    System.out.println("hello jenkins");
   }
 }
